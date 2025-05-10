50b6dafbbb252f0b28bbd5d0bcca022c14101608
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-05-10 13:54:34 +0300

    Prideti member types

--- vector/Vector.hpp
+++ vector/Vector.hpp
@@ -11,6 +11,21 @@
 template<typename T, typename Alloc = std::allocator<T>>
 class Vector {
 
+    // member types
+    using value_type = T;
+    using allocator_type = Alloc;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference = T&;
+    using const_reference = T const&;
+    using pointer = typename std::allocator_traits<Alloc>::pointer;
+    using const_pointer = typename std::allocator_traits<Alloc>::const_pointer;
+    using iterator = T*;
+    using const_iterator = T const*;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+
 };
 
 #endif
