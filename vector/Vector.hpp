829f40f3f1bc2138e022f2e661355d61733197a7
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-05-10 13:27:13 +0300

    Pradeti kurti vector klase

---
+++ vector/Vector.hpp
@@ -0,0 +1,16 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <memory>
+#include <initializer_list>
+#include <iterator>
+#include <algorithm>
+#include <stdexcept>
+#include <utility>
+
+template<typename T, typename Alloc = std::allocator<T>>
+class Vector {
+
+};
+
+#endif
