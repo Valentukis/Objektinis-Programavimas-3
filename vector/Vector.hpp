516ca9666ee40d993b240e5bc981a977c8e5c7c3
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-05-10 14:18:04 +0300

    prideti constructorius

--- vector/Vector.hpp
+++ vector/Vector.hpp
@@ -25,7 +25,54 @@ class Vector {
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+    // constructors
+    Vector() noexcept(std::is_nothrow_default_constructible<Alloc>::value) //default
+        : alloc_(), data_(nullptr), size_(0), cap_(0) {}
 
+    explicit Vector(size_type n, T const& value = T(), Alloc const& a = Alloc()) //size+value
+        : alloc_(a), size_(n), cap_(n)
+    {
+        data_ = alloc_.allocate(cap_);
+        for (size_type i = 0; i < n; ++i)
+        std::allocator_traits<Alloc>::construct(alloc_, data_ + i, value);
+    }
+
+    template<typename InputIt, //range 
+            typename = typename std::enable_if<!std::is_integral<InputIt>::value>::type>
+    Vector(InputIt first, InputIt last, Alloc const& a = Alloc())
+        : alloc_(a)
+    {
+        size_type n = std::distance(first, last);
+        data_ = alloc_.allocate(n);
+        size_ = cap_ = n;
+        std::uninitialized_copy(first, last, data_);
+    }
+
+    Vector(Vector const& other) //copy 
+        : alloc_(std::allocator_traits<Alloc>::select_on_container_copy_construction(other.alloc_)),
+        size_(other.size_), cap_(other.size_)
+    {
+        data_ = alloc_.allocate(cap_);
+        std::uninitialized_copy(other.data_, other.data_ + size_, data_);
+    }
+
+    Vector(Vector&& other) noexcept //move
+        : alloc_(std::move(other.alloc_)),
+        data_(other.data_), size_(other.size_), cap_(other.cap_)
+    {
+        other.data_ = nullptr;
+        other.size_ = other.cap_ = 0;
+    }
+
+    Vector(std::initializer_list<T> il, Alloc const& a = Alloc()) //init list
+        : Vector(il.begin(), il.end(), a) {}
+
+    ~Vector() { //destr
+        clear();
+        if (data_) alloc_.deallocate(data_, cap_);
+    }
+
+    
 };
 
 #endif
